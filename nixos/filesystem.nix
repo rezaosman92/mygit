de9d00f4c965efa45fecdc7f6b44dcd61ec8888e
Reza Maulana <reallyrez@gmail.com>
2022-03-05 13:32:42 +0700

    adding mount point

--- nixos/filesystem.nix
+++ nixos/filesystem.nix
@@ -7,24 +7,27 @@
     { 
       fsType = "btrfs";
       options = ["subvol=@" "noatime" "compress=zstd:3" "space_cache=v2"];
+      mountPoint = "/";
     };
 
   fileSystems."/home" =
     { 
       fsType = "btrfs";
       options = ["subvol=@home" "noatime" "compress=zstd:3" "space_cache=v2"];
+      mountPoint = "/home";
     };
 
   fileSystems."/var/log" =
     { 
       fsType = "btrfs";
       options = ["subvol=@var_log" "noatime" "compress=zstd:3" "space_cache=v2"];
+      mountPoint = "/var/log";
     };
 
   fileSystems."/boot" =
     { 
       fsType = "vfat";
-
+      mountPoint = "/boot";
     };
 
   
