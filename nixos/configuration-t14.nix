d265e93767eb1ec5cf63cb8de8eb1899d3b3a863
Reza Maulana <rezaosman92@gmail.com>
2024-06-27 19:31:01 +0700

    disable nextdns because office sucks ass

--- nixos/configuration-t14.nix
+++ nixos/configuration-t14.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./de-xfce.nix
+      ./de-plasma.nix
       ./filesystem.nix
       ./gpu-amd.nix
       ./packages-t14.nix
@@ -43,17 +43,17 @@
   networking.hostName = "nixos-t14"; 
   networking.networkmanager.enable = true;
 
- services.resolved = {
-   enable = true;
-   dnsovertls = "true";
-   dnssec = "true";
-   extraConfig = "
-                  DNS=45.90.28.0#b6ec66.dns.nextdns.io
-                  DNS=2a07:a8c0::#b6ec66.dns.nextdns.io
-                  DNS=45.90.30.0#b6ec66.dns.nextdns.io
-                  DNS=2a07:a8c1::#b6ec66.dns.nextdns.io
-               ";
- };
+# services.resolved = {
+#   enable = true;
+#   dnsovertls = "true";
+#   dnssec = "true";
+#   extraConfig = "
+#                  DNS=45.90.28.0#b6ec66.dns.nextdns.io
+#                  DNS=2a07:a8c0::#b6ec66.dns.nextdns.io
+#                  DNS=45.90.30.0#b6ec66.dns.nextdns.io
+#                  DNS=2a07:a8c1::#b6ec66.dns.nextdns.io
+#               ";
+# };
 
   ## network configuration end ##
   
