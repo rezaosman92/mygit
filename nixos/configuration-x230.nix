f29fda61f17ae7e2e091b9c7daccc0c3bad1c050
Reza Maulana <rezaosman92@gmail.com>
2024-07-21 17:01:25 +0700

    changes to x230 dns config

--- nixos/configuration-x230.nix
+++ nixos/configuration-x230.nix
@@ -37,9 +37,23 @@
   boot.supportedFilesystems = [ "ntfs" ];
   boot.kernelParams = [ "acpi_backlight=native" ];
 
+  ## network configuration start ##
   networking.hostName = "nixos-x230"; 
   networking.networkmanager.enable = true;
- #networking.wireguard.enable = true;
+  
+# services.resolved = {
+#   enable = true;
+#   dnsovertls = "true";
+#   dnssec = "true";
+#   extraConfig = "
+#           DNS=45.90.28.0#b6ec66.dns.nextdns.io
+#           DNS=2a07:a8c0::#b6ec66.dns.nextdns.io
+#           DNS=45.90.30.0#b6ec66.dns.nextdns.io
+#           DNS=2a07:a8c1::#b6ec66.dns.nextdns.io
+#          ";
+# };
+
+  ## network configuration end ##
 
 # Set your time zone.
   time.timeZone = "Asia/Jakarta";
