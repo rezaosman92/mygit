461b56ee7b6cb798fabcda1ca4bb9d776410a5d3
Reza Maulana <rezaosman92@gmail.com>
2024-08-30 19:25:40 +0700

    create a nix shell for easy rust dev environment

---
+++ nixos/rust-shell.nix
@@ -0,0 +1,16 @@
+let
+
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz")) {};
+
+  # Rolling updates, not deterministic.
+  # pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};
+in pkgs.mkShell {
+  buildInputs = with pkgs; [ 
+  
+  cargo 
+  rustc 
+  rustfmt
+  rust-analyzer
+  
+  ];
+}
