4b53ff31dc7300477d0c7fa4424ce32d2b10843c
Reza Maulana <rezaosman92@gmail.com>
2024-10-18 10:17:16 +0700

    test static ip config for bridge networking (and failed)

--- nixos/virt-manager.nix
+++ nixos/virt-manager.nix
@@ -1,4 +1,4 @@
-{ config, ...}:
+{ config, pkgs, ...}:
 
 {
 programs.virt-manager.enable = true;
@@ -8,5 +8,18 @@ virtualisation.libvirtd = {
     qemu.ovmf.enable = true;
 };
 
+
+ environment.systemPackages = with pkgs; [
+    iproute2
+  ];
+
+#networking.bridges.bridge1.interfaces = [ "wlp3s0" ];
+#networking.interfaces.bridge1 = {
+#    useDHCP = false;
+#    ipv4.addresses = [ {
+#        "address" = "192.168.3.70";
+#        "prefixLength" = 24;
+#    }];
+#};
   
 }
