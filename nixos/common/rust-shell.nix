c06ac13d61c774f6cba28b15a15362cfa64ec165
Reza Maulana <rezaosman92@gmail.com>
2025-02-07 16:40:12 +0700

    change source of rust-shell to 24.11

--- nixos/common/rust-shell.nix
+++ nixos/common/rust-shell.nix
@@ -1,6 +1,6 @@
 let
 
-  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz")) {};
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/refs/tags/24.11.tar.gz")) {};
 
   # Rolling updates, not deterministic.
   # pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};
