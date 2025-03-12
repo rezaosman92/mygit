4220206adbdd3e1ca0684a1b27e4c2be16a00e99
Reza Maulana <rezaosman92@gmail.com>
2025-03-12 19:29:44 +0700

    trying gnome

--- nixos/t14/t14-configuration.nix
+++ nixos/t14/t14-configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./../de/de-plasma.nix
+      ./../de/de-gnome.nix
       ./../filesystem/btrfs.nix
       ./../common/gpu-amd.nix
       ./t14-packages.nix
