ea4f1239707315a350e9cbf1d36b4b4626c3489b
Reza Maulana <rezaosman92@gmail.com>
2025-07-07 09:34:10 +0700

    moved boot-related configs here

---
+++ nixos/common/boot.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+
+{
+
+  # Use the systemd-boot EFI boot loader.
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.memtest86.enable = true;
+
+  boot.loader = {
+    grub = {
+      enable = true;
+      efiSupport = true;
+      memtest86.enable = true;
+      device = "nodev";
+    };
+
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/efi";
+    };
+  };
+
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
+  boot.supportedFilesystems = [ "ntfs" ];
+  boot.kernelParams = [ "acpi_backlight=native" ];
+
+
+}
