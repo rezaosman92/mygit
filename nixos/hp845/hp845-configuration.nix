2f921220bdda3ae6a51699a0ba34771e2b53df9c
Reza Maulana <rezaosman92@gmail.com>
2025-07-07 09:33:27 +0700

    moved boot and gpu configs to another file

--- nixos/hp845/hp845-configuration.nix
+++ nixos/hp845/hp845-configuration.nix
@@ -24,10 +24,6 @@
 
   hardware.cpu.amd.updateMicrocode = true;
   # hardware.cpu.intel.updateMicrocode = true;
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
 
   zramSwap.enable = true;
 
@@ -38,28 +34,6 @@
 
   services.fwupd.enable = true;
   services.colord.enable = true;
-
-  # Use the systemd-boot EFI boot loader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.systemd-boot.memtest86.enable = true;
-
-  boot.loader = {
-    grub = {
-      enable = true;
-      efiSupport = true;
-      memtest86.enable = true;
-      device = "nodev";
-    };
-
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/efi";
-    };
-  };
-
-  boot.kernelPackages = pkgs.linuxPackages_6_12;
-  boot.supportedFilesystems = [ "ntfs" ];
-  boot.kernelParams = [ "acpi_backlight=native" ];
 
 
   networking.hostName = "nixos-hp845";
