51fa5a1e0c872a8dbb66cbec9fe72bff22969e81
Reza Maulana <rezaosman92@gmail.com>
2026-02-12 21:02:28 +0700

    hw-config for t14

--- nixos/t14/hardware-configuration.nix
+++ nixos/t14/hardware-configuration.nix
@@ -8,55 +8,48 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci_renesas" "nvme" "xhci_pci" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci_renesas" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/67ae43b9-320e-4d94-a77a-a71c87cebc54";
+    { device = "/dev/disk/by-uuid/5199fe77-e282-476b-b8b2-9b8f8cbfb8dd";
       fsType = "btrfs";
       options = [ "subvol=@" ];
     };
 
-  fileSystems."/efi" =
-    { device = "/dev/disk/by-uuid/A453-5FB9";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
-
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/67ae43b9-320e-4d94-a77a-a71c87cebc54";
+    { device = "/dev/disk/by-uuid/5199fe77-e282-476b-b8b2-9b8f8cbfb8dd";
       fsType = "btrfs";
       options = [ "subvol=@home" ];
     };
 
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/5199fe77-e282-476b-b8b2-9b8f8cbfb8dd";
+      fsType = "btrfs";
+      options = [ "subvol=@nix" ];
+    };
+
   fileSystems."/var/cache" =
-    { device = "/dev/disk/by-uuid/67ae43b9-320e-4d94-a77a-a71c87cebc54";
+    { device = "/dev/disk/by-uuid/5199fe77-e282-476b-b8b2-9b8f8cbfb8dd";
       fsType = "btrfs";
       options = [ "subvol=@cache" ];
     };
 
   fileSystems."/var/log" =
-    { device = "/dev/disk/by-uuid/67ae43b9-320e-4d94-a77a-a71c87cebc54";
+    { device = "/dev/disk/by-uuid/5199fe77-e282-476b-b8b2-9b8f8cbfb8dd";
       fsType = "btrfs";
       options = [ "subvol=@log" ];
     };
 
-  fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/67ae43b9-320e-4d94-a77a-a71c87cebc54";
-      fsType = "btrfs";
-      options = [ "subvol=@nix" ];
+  fileSystems."/efi" =
+    { device = "/dev/disk/by-uuid/E449-72CE";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices = [ ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
