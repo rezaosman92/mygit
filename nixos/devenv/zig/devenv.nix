ccb7250b59ac0e3b1edde51b2e1eba809e646a04
Reza Maulana <rezaosman92@gmail.com>
2026-02-20 13:46:45 +0700

    modified zig devenv to match rust devenv

--- nixos/devenv/zig/devenv.nix
+++ nixos/devenv/zig/devenv.nix
@@ -4,11 +4,28 @@
   config,
   ...
 }:
+
+let
+  GREET = "You are inside zig devenv";
+in
+
 {
   # https://devenv.sh/languages/
   languages = {
     zig.enable = true;
   };
+
+  packages = with pkgs; [
+    lldb
+
+    cowsay
+    lolcat
+  ];
+
+  enterShell = ''
+    echo ${GREET} | cowsay | lolcat
+    exec fish
+  '';
 
   # See full reference at https://devenv.sh/reference/options/
 }
