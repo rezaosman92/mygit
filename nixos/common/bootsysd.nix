8b1a1f0342030bc3dd522c342e26b7be979eef3b
Reza Maulana <rezaosman92@gmail.com>
2026-02-23 08:04:59 +0700

    removed some comments

--- nixos/common/bootsysd.nix
+++ nixos/common/bootsysd.nix
@@ -2,10 +2,6 @@
 
 {
 
-  # Use the systemd-boot EFI boot loader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.systemd-boot.memtest86.enable = true;
-
   boot.loader.systemd-boot = {
     enable = true;
     memtest86.enable = true;
